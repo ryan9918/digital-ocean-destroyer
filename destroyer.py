89371b3e9d2a92c9453f1fa98c4c3b43d842b0d4
ryan9918 <ryan9918@users.noreply.github.com>
2017-04-02 11:52:34 +0100

    Add files via upload

---
+++ destroyer.py
@@ -0,0 +1,23 @@
+import digitalocean
+import sys
+from time import sleep
+
+token = "" #API TOKEN GOES HERE
+manager = digitalocean.Manager(token=token)
+if not token:
+	print("please make sure you have entered your API token in the program code...")
+	sleep(2)
+	sys.exit()
+my_droplets = manager.get_all_droplets()
+print("fetching droplets...")
+print(my_droplets)
+if not my_droplets:
+    print("no droplets currently active")
+    sleep(2)
+    sys.exit()
+choice = input("destroy all droplets? (y/n):    ")
+if choice == "y":
+    for droplet in my_droplets:
+        droplet.destroy()
+else:
+    sys.exit()
